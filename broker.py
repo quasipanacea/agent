4a7839025a4f08fa150772a47a2806c6cf353e41
Edwin Kofler <edwin@kofler.dev>
2022-08-29 01:51:16 -0700

    wip: basic protocol

--- broker.py
+++ broker.py
@@ -3,10 +3,10 @@ import json
 import struct
 import subprocess
 import sys
+from urllib import request
+import requests
 
-
-# Read a message from stdin and decode it.
-def getMessage():
+def decodeMessage():
     rawLength = sys.stdin.buffer.read(4)
     if len(rawLength) == 0:
         sys.exit(0)
@@ -14,15 +14,13 @@ def getMessage():
     message = sys.stdin.buffer.read(messageLength).decode('utf-8')
     return json.loads(message)
 
-# Encode a message for transmission,
-# given its content.
-def encodeMessage(messageContent):
-    encodedContent = json.dumps(messageContent).encode('utf-8')
-    encodedLength = struct.pack('@I', len(encodedContent))
-    return {'length': encodedLength, 'content': encodedContent}
+def sendMessage(s):
+    def encode(messageContent):
+        encodedContent = json.dumps(messageContent).encode('utf-8')
+        encodedLength = struct.pack('@I', len(encodedContent))
+        return {'length': encodedLength, 'content': encodedContent}
 
-# Send an encoded message to stdout
-def sendMessage(encodedMessage):
+    encodedMessage = encode(s)
     sys.stdout.buffer.write(encodedMessage['length'])
     sys.stdout.buffer.write(encodedMessage['content'])
     sys.stdout.buffer.flush()
@@ -30,35 +28,46 @@ def sendMessage(encodedMessage):
 def launchKaxon():
     subprocess.run(['xdg-open', 'https://localhost:3000'])
 
-with open('broker.log', 'a') as f:
-    while True:
-        receivedMessage = getMessage()
 
-        f.write(receivedMessage + '\n')
-        f.flush()
+while True:
+    message = decodeMessage()
 
-        if ':' in receivedMessage:
-            colon_index = receivedMessage.index(':')
+    with open('broker.log', 'a') as f:
+        f.write(f'message: {json.dumps(message)}\n')
 
-            protocol = receivedMessage[:colon_index]
-            content = receivedMessage[colon_index+1:]
+    if isinstance(message, str):
+        if ':' in message:
+            colon_index = message.index(':')
+
+            protocol = message[:colon_index]
+            content = message[colon_index+1:]
             if protocol == 'yt-checkid':
                 print('fired ' + content)
         else:
-            if receivedMessage == "ping":
-                sendMessage(encodeMessage("pong3"))
-            elif receivedMessage == 'open-kaxon':
+            if message == "ping":
+                sendMessage("pong3")
+            elif message == 'open-kaxon':
                 # launchKaxon()
                 pass
+    elif isinstance(message, dict):
+        typ = message['type'] if 'type' in message else None
+        method = message['method'] if 'method' in message else None
+        data = message['data'] if 'data' in message else None
 
+        if type == None or method == None:
+            f.write('Bad request')
 
-# with open('log', 'w') as f:
-#     while True:
-#         receivedMessage = getMessage()
-#         f.write(len(receivedMessage))
-#         f.flush()
+        if type == 'rpc':
+            if method == 'has-youtube-id':
+                youtubeId = data['id']
 
-# with open('log', 'w') as f:
-#     while True:
-#         for line in fileinput.input():
-#             print("got line:", line)
+                hasNote = True
+                requests.post('http://localhost:3000/', {
+                    type: 'type-unique',
+
+                })
+                sendMessage({
+                    'success': True,
+                    'hasNote': hasNote
+                })
+
